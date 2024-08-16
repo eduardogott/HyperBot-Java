3026f0d37441a1912da3ffe28004aced50945fec
Eduardo Gottert <99107557+eduardogott@users.noreply.github.com>
2024-08-16 20:03:00 -0300

    Fixed DiscordTimestamp

--- src/main/java/me/eduardogottert/discordUtils/DiscordTimestamp.java
+++ src/main/java/me/eduardogottert/discordUtils/DiscordTimestamp.java
@@ -9,9 +9,9 @@ public class DiscordTimestamp {
         LONG_TIME(":T>"),
         SHORT_DATE(":d>"),
         LONG_DATE(":D>"),
-        SHORT_DATE_TIME("f:>"),
-        LONG_DATE_TIME("F:>"),
-        RELATIVE_TIME("R:>");
+        SHORT_DATE_TIME(":f>"),
+        LONG_DATE_TIME(":F>"),
+        RELATIVE_TIME(":R>");
 
         public final String suffix;
 
