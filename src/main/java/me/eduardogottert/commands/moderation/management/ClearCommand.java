2a3f721836ce55469ee3bfc4ae04a1b5e06e5d42
EDUARDO GROHS GÖTTERT <0412951@feevale.br>
2024-08-16 20:43:20 -0300

    Created ClearCommand.java (just a draft)

---
+++ src/main/java/me/eduardogottert/commands/moderation/management/ClearCommand.java
@@ -0,0 +1,88 @@
+package me.eduardogottert.commands.moderation.management;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.exception.MissingPermissionsException;
+import org.javacord.api.listener.message.MessageCreateListener;
+import org.javacord.api.util.logging.ExceptionLogger;
+import org.javacord.api.entity.message.MessageSet;
+import org.javacord.api.entity.channel.TextChannel;
+
+import me.eduardogottert.Main;
+
+public class ClearCommand implements MessageCreateListener {
+
+    private static Logger logger = LogManager.getLogger(ClearCommand.class);
+    private static String[] aliases = {"clear", "purge", "clearchat"};
+    private static String usage = "Usage: !clear {amount} [reason]";
+
+    @Override
+    public void onMessageCreate(MessageCreateEvent event) {
+        String[] splitCommand = event.getMessageContent().split("\\s+");
+
+        if (!Main.parseCommand(aliases, splitCommand[0])) {
+            return;
+        }
+
+        String content = event.getMessageContent();
+        String executor = event.getMessageAuthor().asUser().orElse(null) != null ? event.getMessageAuthor().asUser().orElse(null).getName() + " (" + event.getMessageAuthor().asUser().orElse(null).getId() + ")" : "Unknown User";
+        String channel = event.getChannel().asServerTextChannel().isPresent() ? event.getChannel().asServerTextChannel().get().getName() + " (" + event.getChannel().asServerTextChannel().get().getId() + ")" : "Unknown Channel";
+        logger.info("Command '" + content + "' executed by " + executor + " at #" + channel);
+
+        TextChannel channelToClear = event.getChannel().asTextChannel().get();
+        final int amount;
+        final String reason;
+
+        if (splitCommand.length < 2) {
+            event.getChannel().sendMessage(usage);
+            return;
+        } else if (splitCommand.length < 3) {
+            reason = "Reason not specified";
+        } else {
+            reason = content.substring(content.indexOf(" ", content.indexOf(" ")) + 1);
+        }
+
+        try {
+            amount = Integer.parseInt(splitCommand[1]);
+        } catch (Exception e) {
+            e.printStackTrace();
+            event.getChannel().sendMessage("You didn't specify the amount of messages to be deleted. Perhaps it is not an integer.\n" + usage);
+            return;
+        }
+
+        boolean capped = false;
+        if (amount > 100) {
+            capped = true;
+        }
+
+        if (channelToClear == null) {
+            event.getChannel().sendMessage("Couldn't find the channel to delete messages! Perhaps you are in a DM?");
+            return;
+        }
+
+        MessageSet messagesToDelete;
+        try {
+            messagesToDelete = channelToClear.getMessages(amount).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+            event.getChannel().sendMessage("Couldn't delete the messages.");
+            logger.error("Couldn't delete messages.");
+            return;
+        }
+
+        try {
+            channelToClear.bulkDelete(messagesToDelete).get();
+        } catch (Exception e) {
+            event.getChannel().sendMessage("Couldn't delete all messages. Perhaps some are older than two weeks.");
+            return;
+        }
+
+        if (capped == false) {
+            event.getChannel().sendMessage("Deleted " + amount + " messages.");
+            // TODO add a deleteafter
+        } else {
+            event.getChannel().sendMessage("Deleted 100 messages. Capped due to Discord limitations.");
+        }
+    }
+}
